990111306c649dbc28c6e373a4e36938e0a28721
YU-K <xver456@gmail.com>
2021-05-21 17:18:48 +0400

    Refactor App

---
+++ src/components/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "../container/Container";
+import Header from "../header/Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Container />
+    </div>
+  );
+}
+
+export default App;

4b590d58b5e82eb7749fdd94ffc3611b79f5232c
YU-K <xver456@gmail.com>
2021-05-24 10:49:00 +0400

    Add dummy data

---
+++ src/service/cities.tsx
@@ -0,0 +1,51 @@
+const cities: Array<{}> = [
+  // {
+  //   name: "Samara",
+  //   location: {
+  //     lat: 53.195873,
+  //     lon: 50.100193,
+  //   },
+  // },
+  {
+    Samara: {
+      lat: 53.195873,
+      lon: 50.100193,
+    },
+  },
+  {
+    Tolyatti: {
+      lat: 53.507836,
+      lon: 49.420393,
+    },
+  },
+  // {
+  //   name: "Tolyatti",
+  //   location: {
+  //     lat: 53.507836,
+  //     lon: 49.420393,
+  //   },
+  // },
+  // {
+  //   name: "Saratov",
+  //   location: {
+  //     lat: 51.533557,
+  //     lon: 46.034257,
+  //   },
+  // },
+  // {
+  //   name: "Kazan",
+  //   location: {
+  //     lat: 55.796127,
+  //     lon: 49.106405,
+  //   },
+  // },
+  // {
+  //   name: "Krasnodar",
+  //   location: {
+  //     lat: 45.03547,
+  //     lon: 38.975313,
+  //   },
+  // },
+];
+
+export default cities;

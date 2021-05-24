39b6f6d56206b8819f409b67ff2534f3d921e975
YU-K <xver456@gmail.com>
2021-05-24 10:48:42 +0400

    Add hook and handler
    Added useState for city and onChange handler.
     Added _get method from lodash for getting matched
     city.

--- src/components/daily-weather-block/DailyWeather.tsx
+++ src/components/daily-weather-block/DailyWeather.tsx
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import EmptyData from "../empty-data/EmptyData";
 import SelectCity from "../select-city/SelectCity";
+import WeatherService from "../../service/weather-service";
+import WeatherCard from "../weather-card/WeatherCard";
+import _ from "lodash";
+
+import cities from "../../service/cities";
 
 import "./DailyWeather.css";
+import { FunctionTypeNode } from "typescript";
 
-const DailyWeather = () => {
+// interface DailyWeatherProps {
+//   weather: {
+//     date: string;
+//     image: string;
+//     temp: number;
+//   };
+// }
+
+interface ForecastState {
+  data: "string";
+  image: "string";
+  temp: number;
+}
+
+export const DailyWeather: React.FC<{}> = ({}) => {
+  const weatherService = new WeatherService();
+
+  const [weather, setWeather] = useState<ForecastState | {}>({});
+  const [city, setCity] = useState<string>("");
+
+  // const currentWeather = weatherService.getWeatherForecast(
+  //   53.195873,
+  //   50.100193
+  //   );
+  //   currentWeather.then((data: object) => console.log(data));
+
+  // currentWeather.then((data: object) => setWeather(data));
+
+  // console.log("weather", weather);
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCity(e.currentTarget.value);
+    const location: object = _.get(cities, city);
+    console.log("CityState is ", city);
+    console.log("location", location);
+    console.log("cities", cities);
+  };
+
   return (
     <section className="weather-block">
       <h2 className="weather-block__title">7 Days Foreсast</h2>
-      <SelectCity />
-      <EmptyData />
+      <SelectCity handleChange={handleChange} />
+      {/* <EmptyData /> */}
+      {/* <WeatherCard weather={{ date, image, temp }} /> */}
     </section>
   );
 };

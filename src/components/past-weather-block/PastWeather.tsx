2d24d926fa65797f08eb0aebbed163f5e314eb3d
YU-K <xver456@gmail.com>
2021-05-24 10:44:06 +0400

    Add SelectCity
    Add SelectCity component and declare types for
    PastWeather

--- src/components/past-weather-block/PastWeather.tsx
+++ src/components/past-weather-block/PastWeather.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import EmptyData from "../empty-data/EmptyData";
 import SelectCity from "../select-city/SelectCity";
 
 import "./PastWeather.css";
 
-const PastWeather = () => {
+const PastWeather: React.FC<{}> = () => {
+  const [city, setCity] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCity(e.currentTarget.value);
+  };
+
   return (
     <section className="weather-block">
       <h2 className="weather-block__title">Forecast for a Date in the Past</h2>
-      <SelectCity />
+      <SelectCity handleChange={handleChange} />
       <input className="select" type="date" placeholder="Select date" />
       <EmptyData />
     </section>
